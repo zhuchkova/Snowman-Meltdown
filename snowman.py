8b4521d63474f0f789de5f570c3172734789212d
Ekaterina <ekaterina@zhuchkova.de>
2026-02-28 17:47:56 +0100

    Added initial snowman code: minimal version

---
+++ snowman.py
@@ -0,0 +1,24 @@
+import random
+
+# List of secret words
+WORDS = ["python", "git", "github", "snowman", "meltdown"]
+
+
+def get_random_word():
+    """Selects a random word from the list."""
+    return WORDS[random.randint(0, len(WORDS) - 1)]
+
+
+def play_game():
+    secret_word = get_random_word()
+    print("Welcome to Snowman Meltdown!")
+    print("Secret word selected: " + secret_word)  # for testing, later remove this line
+
+    # TODO: Build your game loop here.
+    # For now, simply prompt the user once:
+    guess = input("Guess a letter: ").lower()
+    print("You guessed:", guess)
+
+
+if __name__ == "__main__":
+    play_game()
